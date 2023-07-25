d9698b83b1846037109ee438b7c51ecc9c32e1df
Supremolink81 <ariozareinia@gmail.com>
2023-07-24 19:49:52 -0700

    Added methods to VoiceManager and Voice class

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from voice_management import VoiceManager
 from typing import Union
+import torch
 
 #voice_manager: VoiceManager = VoiceManager()
 
@@ -11,34 +12,23 @@ def add_state_to_session(state: Union[str, dict], state_value=None) -> None:
 
     Args:
 
-        ```py 
-        Union[str, dict] state
-        ```
-        The state to add. If it is a string, it is interpreted as
+        `Union[str, dict] state`: the state to add. If it is a string, it is interpreted as
         a key and is assigned state_value's value. If it is a dictionary, all key value pairs 
         are inserted as state into the session.
 
-        ```py
-        Any state_value
-        ```
-        
-        the state value to assign to the key, if the state parameter is a string.
+        `Any state_value`: the state value to assign to the key, if the state parameter is a string.
 
     Raises:
 
-        ```py
-        ValueError 
-        ```   
-        
-        in the following cases:
+        `ValueError` in the following cases:
 
-        - `state_value` is not None when state is a dict
+        - `state_value` is not `None` when `state` is a dictionary
 
-        - state_value is None when state is a string
+        - `state_value` is `None` when `state` is a string
 
-        - a type for state other than a string or dictionary is given
+        - a type for `state` other than a string or dictionary is given
 
-        - if state is a dict and any of the keys in state are not strings
+        - if `state` is a dictionary and any of the keys in `state` are not strings
     """
 
     if type(state) == dict:
@@ -73,11 +63,12 @@ def add_state_to_session(state: Union[str, dict], state_value=None) -> None:
 
 if __name__ == "__main__":
 
-    voice_column, text_column = st.columns(2) 
+    voice_column, text_column, audio_file_column = st.columns(3) 
 
-    add_state_to_session("test", [])
-
-    add_state_to_session("audio_path", "")
+    add_state_to_session({
+        "test" : [],
+        "audio_array" : torch.tensor(),
+    })
     
     voice_name: str = st.text_input("Voice Name: ")
 
@@ -90,5 +81,9 @@ if __name__ == "__main__":
         st.write(thing)
 
     st.button("Add Voice", on_click=test_callback)
+
+    with audio_file_column:
+
+        st.audio()
 
     st.write("End of list")
--- voice_management.py
+++ voice_management.py
@@ -1,9 +1,73 @@
 from tortoise_tts.tortoise.api import TextToSpeech
 import torch
+import streamlit as st
+import numpy as np
 from torchaudio.backend.sox_io_backend import load
-from scipy.io.wavfile import write as save_audio
+from functools import total_ordering
 from torchaudio.transforms import Resample
-from streamlit import _DeltaGenerator
+import sortedcontainers
+from PIL import Image
+
+question_mark_image: Image.Image = Image.open("questionmark.png")
+
+@total_ordering
+class Voice:
+
+    """
+    Data class for voices.
+
+    Fields:
+
+        `str name`: the name of the voice.
+
+        `tuple[torch.Tensor, torch.Tensor] latent_vectors`: the latent vectors 
+        of teh voice; necessary for Tortoise TTS.
+
+        `Image.Image profile_picture`: the profile picture of the voice. Used
+        for rendering.
+    """
+
+    name: str
+    latent_vectors: tuple[torch.Tensor, torch.Tensor]
+    profile_picture: Image.Image
+
+    def __init__(self, name: str, latent_vectors: tuple[torch.Tensor, torch.Tensor], profile_picture: Image.Image):
+
+        self.name = name
+
+        self.latent_vectors = latent_vectors
+
+        self.profile_picture = profile_picture
+
+    def __hash__(self):
+
+        return (self.name.__hash__() ^ self.latent_vectors.__hash__()) + self.profile_picture.__hash__()
+    
+    def check_if_other_is_voice(self, other):
+
+        if not isinstance(other, Voice):
+
+            raise ValueError("Tried to compare a Voice object to a non-Voice object.")
+    
+    def __eq__(self, other) -> bool:
+
+        """
+        Returns true if both Voice objects have the same name and false otherwise.
+        """
+
+        self.check_if_other_is_voice(other)
+        
+        return self.name == other.name
+    
+    def __lt__(self, other) -> bool:
+
+        """
+        Compares two Voice objects by lexicographical order of their names.
+        """
+
+        self.check_if_other_is_voice(other)
+
+        return self.name < other.name
 
 class VoiceManager:
 
@@ -14,15 +78,20 @@ class VoiceManager:
 
     Fields:
 
-        dict[str, tuple[torch.Tensor, torch.Tensor]] voices: the dictionary to map
+        `dict[str, tuple[torch.Tensor, torch.Tensor]] voices`: the dictionary to map
         voice names to their latent vectors, which are necessary for voice cloning.
 
-        TextToSpeech text_to_speech: the Tortoise class used for text-to-speech.
+        `TextToSpeech text_to_speech`: the Tortoise class used for text-to-speech.
+
+        `sortedcontainers.SortedSet voice_name_set`: an ordered set of voice names;
+        used for easy ordering when rendering voices on Streamlit.
     """
 
     voices: dict[str, tuple[torch.Tensor, torch.Tensor]]
 
     text_to_speech: TextToSpeech
+
+    voice_name_set: sortedcontainers.SortedSet[Voice]
 
     def __init__(self):
 
@@ -30,16 +99,108 @@ class VoiceManager:
 
         self.text_to_speech = TextToSpeech()
 
-    def add_voice(self, voice_name: str, voice_recording_paths: list[str]):
+        self.voice_name_set = sortedcontainers.SortedSet()
+
+    def add_voice(self, voice_name: str, voice_recording_paths: list[str], profile_picture: Image.Image = question_mark_image):
 
         """
         Adds a voice to the database, if it doesn't already exist.
 
         Args:
 
-            str voice_name: the name to give the voice.
+            `str voice_name`: the name to give the voice.
 
-            list[str] voice_recording_paths: the paths to the voice recordings to use to clone the voice.
+            `list[str] voice_recording_paths`: the paths to the voice recordings to use to clone the voice.
+
+            `Image.Image profile_picture`: the profile picture to give the voice. Default: question mark image.
+        """
+
+        recording_tensors: list[torch.Tensor] = self._get_recording_tensors(voice_recording_paths)
+
+        latent_vectors: tuple[torch.Tensor, torch.Tensor] = self.text_to_speech.get_conditioning_latents(recording_tensors)
+
+        self.voices[voice_name] = latent_vectors
+
+        voice_object: Voice = Voice(voice_name, latent_vectors, profile_picture)
+
+        self.voice_name_set.add(voice_object)
+
+    def delete_voice(self, voice_name: str):
+
+        """
+        Deletes a voice from the database, if it exists.
+
+        Args:
+
+            `str voice_name`: the name to give the voice.
+        """
+
+        if voice_name in self.voices:
+
+            del self.voices[voice_name]
+
+    def text_to_speech(self, voice_name: str, text: str) -> torch.Tensor:
+
+        """
+        Converts a given text into speech in the given voice.
+
+        Args:
+
+            `str voice_name`: the voice to use.
+
+            `str text`: the text to convert to speech.
+
+        Returns:
+
+            a tensor representing the audio generated.
+
+        Raises:
+
+            `KeyError` if the voice name is not in the VoiceManager.
+        """
+
+        try:
+
+            voice_latent_vectors: tuple[torch.Tensor, torch.Tensor] = self.voices[voice_name]
+
+            audio_tensor: torch.Tensor = self.text_to_speech.tts(text, conditioning_latents=voice_latent_vectors)
+
+            return audio_tensor
+
+        except KeyError:
+
+            raise KeyError("Voice not found: " + voice_name)
+        
+    def render_voices(self):
+
+        """
+        Function to render list of voices in a Streamlit application.
+
+        Args:
+            
+            `None
+        """
+
+        for voice in self.voice_name_set:
+
+            profile_picture_as_array: np.array = np.array(voice.profile_picture)
+
+            st.write(voice.name)
+
+            st.image(profile_picture_as_array)
+
+    def _get_recording_tensors(self, voice_recording_paths: list[str]) -> list[torch.Tensor]:
+
+        """
+        Obtains the recording tensors for a list of voice recording paths.
+
+        Args:
+
+            `list[str] voice_recording_paths`: the paths to convert.
+
+        Returns:
+
+            a list of tensors corresponding to the audio data.
         """
 
         recording_tensors: list[torch.Tensor] = []
@@ -59,61 +220,4 @@ class VoiceManager:
 
             recording_tensors.append(recording_tensor)
 
-        latent_vectors: tuple[torch.Tensor, torch.Tensor] = self.text_to_speech.get_conditioning_latents(recording_tensors)
-
-        self.voices[voice_name] = latent_vectors
-
-    def delete_voice(self, voice_name: str):
-
-        """
-        Deletes a voice from the database, if it exists.
-
-        Args:
-
-            str voice_name: the name to give the voice.
-        """
-
-        if voice_name in self.voices:
-
-            del self.voices[voice_name]
-
-    def text_to_speech(self, voice_name: str, text: str, audio_file_path: str):
-
-        """
-        Converts a given text into speech in the given voice and saves it as a waveform file.
-
-        Args:
-
-            str voice_name: the voice to use.
-
-            str text: the text to convert to speech.
-
-            str audio_file_path: the path to save the audio to. 
-        """
-
-        try:
-
-            voice_latent_vectors: tuple[torch.Tensor, torch.Tensor] = self.voices[voice_name]
-
-            audio_tensor: torch.Tensor = self.text_to_speech.tts(text, conditioning_latents=voice_latent_vectors)
-
-            # sample rate of generated speech is 24 kHz in tortoise backend
-            save_audio(audio_file_path, 24000, audio_tensor)
-
-        except KeyError:
-
-            raise KeyError("Voice not found: " + voice_name)
-        
-    def render_voices(self, column: _DeltaGenerator):
-
-        """
-        Function to render list of voices in a Streamlit application.
-
-        Args:
-
-            ```py
-            _DeltaGenerator column
-            ```
-
-            The column to render the voices to.
-        """
+        return recording_tensors
